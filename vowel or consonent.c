62fcd361038cc630c48abb4fe0e3f84b555288af
Salma2606 <47620975+Salma2606@users.noreply.github.com>
2019-03-21 10:33:15 +0530

    Create vowel or consonent.c

---
+++ vowel or consonent.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+  char ch;
+ 
+  printf("Enter a character\n");
+  scanf("%c", &ch);
+ 
+  // Checking both lower and upper case
+ 
+  if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
+    printf("%c vowel.\n", ch);
+  else 
+    printf("%c consonent.\n", ch);
+  return 0;
+}
